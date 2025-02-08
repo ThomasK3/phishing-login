84668a3aebc4527fe4b70b240b523956459d3e99
Thomas <70901054+ThomasK3@users.noreply.github.com>
2025-02-08 13:40:43 +0100

    Updates

--- backend/src/routes/credential.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { credentialController } from '../controllers/credential.controller';
-
-const router = Router();
-
-router.post('/', credentialController.create);
-
-export default router;
